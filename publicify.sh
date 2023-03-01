7a48c3e2e821e34e93c8d2be5d8a4f6e3e9c027e
Shaun Delaney <shaun@waveform.gg>
2023-03-01 18:51:32 -0500

    Publicify helper script

---
+++ publicify.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Colors
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+# The folder to copy
+dir="public"
+
+echo ""
+
+# Get all project directories
+for d in src/projects/*/; do
+  # Save names for folder manipulations
+  _d=$(basename $d)
+  pathPublic=./$dir/$_d
+  pathProject=$d$dir
+
+  # Check for a public folder
+  if [ ! -d $pathProject ]
+  then
+    echo -e "${RED}$_d${NC} does not have a public folder, ${BLUE}skipping${NC}..."
+    continue
+  fi
+
+  # Count number of files
+  numFiles=$(ls -1 $d$dir | wc -l)
+
+  # If zero files, continue
+  if [ $numFiles -eq 0 ]
+  then
+    continue
+  fi
+
+  echo -e "$RED$_d$NC has a public folder, moving $YELLOW$numFiles$NC files/folders to $BLUE$pathPublic$NC..."
+
+  # Files exist
+  # Copy over the public folder
+  mkdir -p $pathPublic
+  cp -r $pathProject/* $pathPublic
+done
+
+echo ""
